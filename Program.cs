38e2dfd7ee30be502edc29d77fea59d89d048307
Evgeniy <spardasenpai@gmail.com>
2022-06-26 17:27:33 +0300

    DZ

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3 */
+int M = 5;
+int count = 0;
+while (  == )
+{
+    Console.WriteLine("Введите число");
+    int f = int.Parse(Console.ReadLine());
+    count++;
+    
+}
+ Console.WriteLine(count); */
+///////////////////////////////////////////////
+
+/* Задача 43. Напишите программу, которая найдёт точку
+пересечения двух прямых, заданных уравнениями
+ y = k1 *x + b1,
+ y = k2 * x + b2;
+  значения b1, k1, b2 и k2 задаются
+пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 =
+9 -> (-0,5; 5,5) */
+
+
+/* Console.WriteLine("Введите b1");
+double b1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите k1");
+double k1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите b2");
+double b2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите k2");
+double k2 = int.Parse(Console.ReadLine());
+
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x  + b1; 
+
+
+
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
+*/
